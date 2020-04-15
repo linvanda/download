a0ca99d069be078a17ec60388e91ea425de599ca
songlin.zhang <songlin.zhang@weicheche.cn>
2020-04-15 16:55:39 +0800

    c

--- app/Http/Routes/Routes.php
+++ app/Http/Routes/Routes.php
@@ -10,5 +10,6 @@ class Routes extends Route
     {
         $this->get("/v1/test", "/V1/Test/index");
         $this->get("/v1/test/download", "/V1/Test/download");
+        $this->get("/v1/test/create", "/V1/Test/createBigFile");
     }
 }
